d07840b9da85621eacd1d8a8e1abe99bbd2a9bc9
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-03-14 01:50:27 +0300

    login design Edited and binding activity main added

--- app/src/main/java/com/asimodabas/uni_chat/MainActivity.kt
+++ app/src/main/java/com/asimodabas/uni_chat/MainActivity.kt
@@ -2,10 +2,16 @@ package com.asimodabas.uni_chat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.asimodabas.uni_chat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
