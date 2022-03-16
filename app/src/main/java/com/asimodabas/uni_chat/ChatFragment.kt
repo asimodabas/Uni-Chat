46464e4e9ed8e9c3f9cd42c42336e4ec8cc941be
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-03-16 05:50:01 +0300

    Message sending to server completed

--- app/src/main/java/com/asimodabas/uni_chat/ChatFragment.kt
+++ app/src/main/java/com/asimodabas/uni_chat/ChatFragment.kt
@@ -9,6 +9,9 @@ import com.asimodabas.uni_chat.databinding.FragmentChatBinding
 import com.asimodabas.uni_chat.databinding.FragmentLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class ChatFragment : Fragment() {
@@ -16,12 +19,15 @@ class ChatFragment : Fragment() {
     private var _binding: FragmentChatBinding? = null
     private val binding get() = _binding!!
     private lateinit var auth: FirebaseAuth
+    private lateinit var firestore : FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setHasOptionsMenu(true)
+
         auth=Firebase.auth
+        firestore = Firebase.firestore
 
     }
 
@@ -39,6 +45,24 @@ class ChatFragment : Fragment() {
 
         binding.sendButton.setOnClickListener {
 
+            auth.currentUser?.let {
+                val user = it.email
+                val chatText = binding.chatText.text.toString()
+                val date = FieldValue.serverTimestamp()
+
+                val dataMap=HashMap<String,Any>()
+
+                dataMap.put("text",chatText)
+                dataMap.put("user",user!!)
+                dataMap.put("date",date)
+
+                firestore.collection("Public-Chat").add(dataMap).addOnSuccessListener {
+                    binding.chatText.setText("")
+                }.addOnFailureListener {
+                    Toast.makeText(requireContext(),it.localizedMessage,Toast.LENGTH_LONG).show()
+                    binding.chatText.setText("")
+                }
+            }
         }
 
     }
