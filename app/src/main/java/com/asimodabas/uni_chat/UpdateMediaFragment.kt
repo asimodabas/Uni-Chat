e18f505c2f1983871e1e43285855dc7202c00160
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-05-04 18:40:21 +0300

    gallery permission

--- app/src/main/java/com/asimodabas/uni_chat/UpdateMediaFragment.kt
+++ app/src/main/java/com/asimodabas/uni_chat/UpdateMediaFragment.kt
@@ -1,20 +1,40 @@
 package com.asimodabas.uni_chat
 
+import android.app.Activity
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.asimodabas.uni_chat.databinding.FragmentChatBinding
 import com.asimodabas.uni_chat.databinding.FragmentUpdateMediaBinding
+import com.google.android.material.snackbar.Snackbar
+import java.lang.Exception
+import java.util.jar.Manifest
 
 class UpdateMediaFragment : Fragment() {
 
     private var _binding: FragmentUpdateMediaBinding? = null
     private val binding get() = _binding!!
+    var selectedImage : Uri? = null
+    var selectedBitmap : Bitmap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
     }
 
@@ -30,9 +50,60 @@ class UpdateMediaFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
+        binding.uploadImageView.setOnClickListener {
+            selected_image(it)
+        }
     }
 
+    fun selected_image(view: View){
+        activity?.let {
+            if (ContextCompat.checkSelfPermission(it.applicationContext,android.Manifest.permission.READ_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1)
+            }else{
+                val Intent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(Intent,2)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 1){
+            if (grantResults.size>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                val Intent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(Intent,2)
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == 2 && resultCode == Activity.RESULT_OK && data != null){
+            selectedImage = data.data
+        }
+        try {
+
+            context?.let {
+                if (selectedImage!=null){
+                    if (Build.VERSION.SDK_INT >= 28){
+                        val source = ImageDecoder.createSource(it.contentResolver,selectedImage!!)
+                        selectedBitmap = ImageDecoder.decodeBitmap(source)
+                        binding.uploadImageView.setImageBitmap(selectedBitmap)
+                    }else{
+                        selectedBitmap = MediaStore.Images.Media.getBitmap(it.contentResolver,selectedImage)
+                        binding.uploadImageView.setImageBitmap(selectedBitmap)
+                    }
+                }
+            }
+
+        }catch (e:Exception){
+            e.printStackTrace()
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
