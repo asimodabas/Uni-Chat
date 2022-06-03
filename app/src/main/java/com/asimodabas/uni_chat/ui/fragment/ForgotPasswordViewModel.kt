4b20d8d334a96556cf3cc803e6d8361885ae6730
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-06-04 01:16:21 +0300

    forgot password viewmodel added

--- app/src/main/java/com/asimodabas/uni_chat/ui/fragment/ForgotPasswordViewModel.kt
+++ app/src/main/java/com/asimodabas/uni_chat/ui/fragment/ForgotPasswordViewModel.kt
@@ -1,9 +1,33 @@
 package com.asimodabas.uni_chat.ui.fragment
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class ForgotPasswordViewModel : ViewModel() {
 
+    private val auth = Firebase.auth
+
+    val animation = MutableLiveData<Boolean>()
+    val errorMessage = MutableLiveData<String>()
+    val success = MutableLiveData<Boolean>()
+
+    fun forgotPassword(email: String) {
+        forgotPasswordFirebase(email)
+    }
+
+    private fun forgotPasswordFirebase(email: String) {
+        animation.value = true
+        auth.sendPasswordResetEmail(email).addOnCompleteListener { process ->
+            if (process.isSuccessful) {
+                success.value = true
+                animation.value = false
+            }
+        }.addOnFailureListener { error ->
+            animation.value = false
+            errorMessage.value = error.localizedMessage
+        }
+    }
 
 }
